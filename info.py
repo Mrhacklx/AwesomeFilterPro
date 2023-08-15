d534b0d0125ce92d160fbee4fd717680188efe5d
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2023-08-15 13:20:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mrhacklx:mrhacklx@7111@cluster0.hormkep.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb srv://leecher-x:#DiViN143#@cluster8.e9vvmrg.mongodb.net/?retrywrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
